ded0a7e78854d9cc8a9b9b1bf043a7085d493dc0
Tsilko B <boants@gmail.com>
2025-05-13 19:49:26 +0700

    file for articledto

--- Controllers/ArticleController.cs
+++ Controllers/ArticleController.cs
@@ -132,4 +132,5 @@ public class ArticleDto
     public string Content { get; set; }
     public string Category { get; set; }
     public List<string> Tags { get; set; }
+    public IFormFile File { get; set; } 
 }
